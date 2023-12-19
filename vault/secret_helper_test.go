7ef21d3a08422defca969da50eb82033b06cc8f1
Matt Schuchard <grapheneowns@gmail.com>
2023-12-19 17:09:19 -0500

    update unit tests for refactor

--- vault/secret_helper_test.go
+++ vault/secret_helper_test.go
@@ -50,29 +50,33 @@ func TestRetrieveKVSecret(test *testing.T) {
 	}
 }
 
-// test populate secret
-func TestPopulateKVSecret(test *testing.T) {
+// test populate kv1 secret
+func TestPopulateKV1Secret(test *testing.T) {
 	basicVaultClient := basicVaultConfig.AuthClient()
 
 	kv1VaultSecret := NewVaultSecret("kv1", "", KVPath)
-	version, secretMetadata, err := kv1VaultSecret.PopulateKVSecret(
+	version, secretMetadata, err := kv1VaultSecret.populateKV1Secret(
 		basicVaultClient,
 		map[string]interface{}{KVKey: KVValue},
-		false,
 	)
 	if err != nil {
 		test.Error("the kv1 secret was not successfully put")
 		test.Error(err)
 	}
 	if secretMetadata == (Metadata{}) {
-		test.Error("the kv2 secret retrieval returned empty metadata")
+		test.Error("the kv1 secret retrieval returned empty metadata")
 	}
 	if version != "0" {
 		test.Errorf("the kv1 secret put returned non-zero version: %s", version)
 	}
+}
+
+// test populate kv2 secret
+func TestPopulateKV2Secret(test *testing.T) {
+	basicVaultClient := basicVaultConfig.AuthClient()
 
 	kv2VaultSecret := NewVaultSecret("kv2", "", KVPath)
-	version, secretMetadata, err = kv2VaultSecret.PopulateKVSecret(
+	version, secretMetadata, err := kv2VaultSecret.populateKV2Secret(
 		basicVaultClient,
 		map[string]interface{}{KVKey: KVValue},
 		false,
@@ -87,7 +91,7 @@ func TestPopulateKVSecret(test *testing.T) {
 	if version == "0" {
 		test.Errorf("the kv2 secret put returned an invalid version: %s", version)
 	}
-	version, secretMetadata, err = kv2VaultSecret.PopulateKVSecret(
+	version, secretMetadata, err = kv2VaultSecret.populateKV2Secret(
 		basicVaultClient,
 		map[string]interface{}{"other_password": "ultrasecret"},
 		true,
