92cec64301813fd664a67150125842083d124896
Matt Schuchard <grapheneowns@gmail.com>
2024-11-14 09:59:16 -0500

    update unit tests for refactor

--- vault/client_test.go
+++ vault/client_test.go
@@ -4,23 +4,25 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/mschuchard/concourse-vault-resource/concourse"
 	"github.com/mschuchard/concourse-vault-resource/vault/util"
 )
 
 var (
-	basicVaultConfig = &VaultConfig{
+	basicSourceConfig = concourse.Source{
 		Address: util.VaultAddress,
 		Token:   util.VaultToken,
 	}
-	awsVaultConfig = &VaultConfig{
-		Address: "https://192.168.9.10",
-		AWSRole: "myIAMRole",
+	awsSourceConfig = concourse.Source{
+		Address:      "https://192.168.9.10",
+		AWSVaultRole: "myIAMRole",
 	}
 )
 
 // test config constructor
 func TestNewVaultConfig(test *testing.T) {
-	if err := basicVaultConfig.New(); err != nil {
+	vaultConfig, err := NewVaultConfig(basicSourceConfig)
+	if err != nil {
 		test.Error("the basic vault config did not successfully validate")
 		test.Error(err)
 	}
@@ -31,13 +33,14 @@ func TestNewVaultConfig(test *testing.T) {
 		Insecure: true,
 	}
 
-	if *basicVaultConfig != expectedVaultConfig {
+	if *vaultConfig != expectedVaultConfig {
 		test.Error("the vault basic config constructor returned unexpected values.")
 		test.Errorf("expected vault config: %v", expectedVaultConfig)
-		test.Errorf("actual vault config: %v", *basicVaultConfig)
+		test.Errorf("actual vault config: %v", *vaultConfig)
 	}
 
-	if err := awsVaultConfig.New(); err != nil {
+	vaultConfig, err = NewVaultConfig(awsSourceConfig)
+	if err != nil {
 		test.Error("the aws vault config did not successfully validate")
 		test.Error(err)
 	}
@@ -48,53 +51,61 @@ func TestNewVaultConfig(test *testing.T) {
 		AWSRole:      "myIAMRole",
 	}
 
-	if *awsVaultConfig != expectedVaultConfig {
+	if *vaultConfig != expectedVaultConfig {
 		test.Error("the vault aws config constructor returned unexpected values.")
 		test.Errorf("expected vault config: %v", expectedVaultConfig)
-		test.Errorf("actual vault config: %v", *awsVaultConfig)
+		test.Errorf("actual vault config: %v", *vaultConfig)
 	}
 
-	invalidServerConfig := VaultConfig{Address: "https//:foo.com"}
-	if err := invalidServerConfig.New(); err == nil || err.Error() != "invalid Vault server address" {
+	invalidServerConfig := concourse.Source{Address: "https//:foo.com"}
+	if _, err := NewVaultConfig(invalidServerConfig); err == nil {
 		test.Error("invalid vault server address did not error as expected")
 	}
 
-	ambiguousAuth := VaultConfig{
+	ambiguousAuth := concourse.Source{
 		Token:        util.VaultToken,
 		AWSMountPath: "gcp",
 	}
-	if err := ambiguousAuth.New(); err == nil || err.Error() != "unable to deduce authentication engine" {
+	if _, err := NewVaultConfig(ambiguousAuth); err == nil || err.Error() != "unable to deduce authentication engine" {
 		test.Error("ambiguous unspecified authentication engine did not error as expected")
 	}
 
-	invalidAuth := VaultConfig{Engine: "does not exist"}
-	if err := invalidAuth.New(); err == nil || err.Error() != "invalid Vault authentication engine" {
+	invalidAuth := concourse.Source{AuthEngine: "does not exist"}
+	if _, err := NewVaultConfig(invalidAuth); err == nil || err.Error() != "invalid Vault authentication engine" {
 		test.Error("invalid authentication engine did not error as expected")
 	}
 
-	invalidToken := VaultConfig{Token: "foobarbaz"}
-	if err := invalidToken.New(); err == nil || err.Error() != "invalid vault token" {
+	invalidToken := concourse.Source{Token: "foobarbaz"}
+	if _, err := NewVaultConfig(invalidToken); err == nil || err.Error() != "invalid vault token" {
 		test.Error("invalid vault token did not error as expected")
 	}
 }
 
 // test client token authentication
 func TestAuthClient(test *testing.T) {
-	basicVaultConfig.New()
-	basicClient, err := basicVaultConfig.AuthClient()
+	vaultConfig, err := NewVaultConfig(basicSourceConfig)
+	if err != nil {
+		test.Error("the basic vault config did not successfully validate")
+		test.Error(err)
+	}
+	basicClient, err := NewClient(vaultConfig)
 	if err != nil {
 		test.Error("authenticating a vault client with a basic token config errored")
 		test.Error(err)
 	}
-	if basicClient.Address() != basicVaultConfig.Address || basicClient.Token() != basicVaultConfig.Token {
+	if basicClient.Address() != basicSourceConfig.Address || basicClient.Token() != basicSourceConfig.Token {
 		test.Error("the authenticated Vault client return failed basic validation")
-		test.Errorf("expected Vault token: %s, actual: %s", basicVaultConfig.Token, basicClient.Token())
-		test.Errorf("expected Vault address: %s, actual: %s", basicVaultConfig.Address, basicClient.Address())
+		test.Errorf("expected Vault token: %s, actual: %s", basicSourceConfig.Token, basicClient.Token())
+		test.Errorf("expected Vault address: %s, actual: %s", basicSourceConfig.Address, basicClient.Address())
 	}
 
-	awsVaultConfig.Address = util.VaultAddress
-	awsVaultConfig.New()
-	if _, err = awsVaultConfig.AuthClient(); err == nil || !strings.Contains(err.Error(), "NoCredentialProviders: no valid providers in chain") {
+	awsSourceConfig.Address = util.VaultAddress
+	vaultConfig, err = NewVaultConfig(awsSourceConfig)
+	if err != nil {
+		test.Error("the aws vault config did not successfully validate")
+		test.Error(err)
+	}
+	if _, err = NewClient(vaultConfig); err == nil || !strings.Contains(err.Error(), "NoCredentialProviders: no valid providers in chain") {
 		test.Error("authenticating a vault client with aws did not error in the expected manner")
 		test.Errorf("expected error (contains): NoCredentialProviders: no valid providers in chain, actual: %v", err)
 	}
