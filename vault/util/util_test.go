6a1b1352c504b93deefe6607277df84bff0815bf
Matthew Schuchard <8029687+mschuchard@users.noreply.github.com>
2026-03-09 17:08:33 -0400

    update and fix bootstrapping for better unit tests

--- vault/util/util_test.go
+++ vault/util/util_test.go
@@ -11,12 +11,19 @@ import (
 func TestBootstrap(test *testing.T) {
 	// check if we should skip bootstrap
 	auths, _ := VaultClient.Sys().ListAuth()
-	if _, ok := auths["auth/aws/"]; ok {
+	if _, ok := auths["kubernetes/"]; ok {
 		test.Skip("Vault server already bootstrapped; skipping")
 	}
 
-	// enable auth: aws
-	VaultClient.Sys().EnableAuthWithOptions("auth/aws", &vault.EnableAuthOptions{Type: "aws"})
+	// enable auth: approle, aws, kubernetes (token enabled by default with dev server)
+	VaultClient.Sys().EnableAuthWithOptions("approle", &vault.EnableAuthOptions{Type: "approle"})
+	VaultClient.Logical().Write("auth/approle/role/myAppRole", map[string]any{
+		"token_policies": "default",
+		"token_ttl":      "1h",
+		"token_max_ttl":  "4h",
+	})
+	VaultClient.Sys().EnableAuthWithOptions("aws", &vault.EnableAuthOptions{Type: "aws"})
+	VaultClient.Sys().EnableAuthWithOptions("kubernetes", &vault.EnableAuthOptions{Type: "kubernetes"})
 
 	// enable secrets: database, aws, kv1 (kv2 enabled by default with dev server)
 	VaultClient.Sys().Mount("aws/", &vault.MountInput{Type: "aws"})
@@ -30,17 +37,17 @@ func TestBootstrap(test *testing.T) {
 	VaultClient.KVv1(KV1Mount).Put(
 		context.Background(),
 		KVPath,
-		map[string]interface{}{KVKey: KVValue},
+		map[string]any{KVKey: KVValue},
 	)
 	VaultClient.KVv2(KV2Mount).Put(
 		context.Background(),
 		KVPath,
-		map[string]interface{}{KVKey: KVValue, "other_password": "ultrasecret"},
+		map[string]any{KVKey: KVValue, "other_password": "ultrasecret"},
 	)
 	// for full "in" test
 	VaultClient.KVv2(KV2Mount).Put(
 		context.Background(),
 		"bar/baz",
-		map[string]interface{}{KVKey: KVValue},
+		map[string]any{KVKey: KVValue},
 	)
 }
